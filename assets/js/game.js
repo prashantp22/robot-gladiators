8bc65c0c69656ca820f4126396d84c8923564576
PrashantP <prashantpatel622@gmail.com>
2022-01-12 17:35:33 -0500

    randomness

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,13 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+// function to generate a random numeric value
+var randomNumber = function(min,max) {
+    var value = Math.floor(Math.random()  * (max - min + 1) + min);
+  
+    return value;
+};
+
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd like to fight or run
@@ -33,7 +40,11 @@ var fight = function(enemyName) {
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = Math.max(0, enemyHealth - playerAttack);
+      // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -52,7 +63,10 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = Math.max(0, playerHealth - enemyAttack);
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -84,7 +98,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
         
             // reset enemyHealth before starting new fight
-            enemyHealth = Math.floor(Math.random() * 21) + 40;
+            enemyHealth = randomNumber(40,60);
         
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
